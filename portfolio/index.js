cfd98483ef273ae931f0e1c9f3f7a7db3e33278b
ctien <phamcongtienpro01@gmail.com>
2019-06-22 18:26:30 +0700

    modify portfolio

--- portfolio/index.js
+++ portfolio/index.js
@@ -4,4 +4,16 @@ $('body').ready(() => {
         'opacity': '1',
         'top': '40%'
     })
+    $('.btn-language').click(() => {
+        if($('.btn-language').hasClass('btn-language--vn')){
+            $('.btn-language').removeClass('btn-language--vn');
+            $('.btn-language').addClass('btn-language--en');
+            $('.btn-language').text('En');
+        }
+        else{
+            $('.btn-language').removeClass('btn-language--en');
+            $('.btn-language').addClass('btn-language--vn');
+            $('.btn-language').text('Vn');
+        }
+    })
 })
